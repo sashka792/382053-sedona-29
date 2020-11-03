1cccd5f7acc4e561112e2b300f0afb459097d9c8
sashka792 <sashka792@gmail.com>
2020-11-03 15:15:06 +0200

    Доработки по форме

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const dateIn = form.querySelector("[name=check-in]");
 const dateOut = form.querySelector("[name=check-out]");
 const adultsNum = form.querySelector("[name=adults]");
 const childrenNum = form.querySelector("[name=children]");
+const findButton = form.querySelector(".registration-form_find");
 
 let isStorageSupport = true;
 let adultsStorage = "";
@@ -12,25 +13,37 @@ let childrenStorage = "";
 
 try {
   adultsStorage = localStorage.getItem("adults");
+} catch (err) {
+  isStorageSupport = false; 
+}
+
+try {
   childrenStorage = localStorage.getItem("children");
 } catch (err) {
   isStorageSupport = false; 
 }
 
+// buttonSearch.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   form.classList.toggle("d-none");
+// }); 
+
 buttonSearch.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (form.classList.contains("registration-form")){
     form.classList.remove("registration-form");
+    form.classList.remove("d-block");
     form.classList.remove("error");
     form.classList.add("d-none");
   } else {
     form.classList.remove("d-none");
     form.classList.add("registration-form");
+    form.classList.add("d-block");
     dateIn.focus();
   }
 }); 
 
-form.addEventListener("submit", function (evt){
+findButton.addEventListener("click", function (evt){
     if (!adultsNum.value || !childrenNum.value) {
         evt.preventDefault();
         form.classList.remove("error");
